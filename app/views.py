ccf36196e71186c5169504696106185f5e31c371
ctlnwtkns <ctlnwtkns@gmail.com>
2016-03-20 19:02:17 -0700

    ...hard to say

--- app/views.py
+++ app/views.py
@@ -1,8 +1,5 @@
-from flask import Flask, render_template, flash, redirect, url_for
-
-app = Flask(__name__)
-app.config.from_object('config')
-
+from flask import render_template, flash, redirect, url_for, request, session
+from app import app
 from .forms import LoginForm
 
 @app.route('/')
@@ -12,22 +9,24 @@ def index():
     form = LoginForm()
     error = None
     if request.method == 'POST':
-        if request.form['username'] != None:
+        if request.form['name'] == None:
             error = 'Please enter your name.'
         else:
             return redirect('/greet')
     if form.validate_on_submit():
-       #flash('Login requested for OpenID="%s"' % #form.openid.data)
+        flash('Login requested for OpenID="%s"' % form.openid.data)
         return redirect('/greet')
     return render_template('index.html', 
                             title='Index',
-                           form=form
+                            form=form
                            )
 
 @app.route('/greet')
 def greet():
-    return render_template('greet.html', 
-    						title='Greetings'
+    name = 'Caitlin'
+    return render_template( 'greet.html', 
+    						title='Greetings',
+                            name=name
                           )
 
 @app.route('/intro')
@@ -42,8 +41,11 @@ def hatchet():
 
 @app.route('/apples')
 def apples():
+    name = 'Caitlin'
     return render_template('apples.html',
-                            title = 'Apples')
+                            title = 'Apples',
+                            name=name
+                          )
 
 @app.route('/river')
 def river():
