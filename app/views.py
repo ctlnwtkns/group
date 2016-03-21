3985001e28fc6c56760a4b43f5296b633274f5c1
ctlnwtkns <ctlnwtkns@gmail.com>
2016-03-21 00:11:17 -0700

    add counter

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
 from .forms import LoginForm
+from collections import Counter
+import random
+import time
+
+c = Counter({'health': 100, 'hunger': 0, 'stash': ''})
 
 @app.route('/')
 def home():
@@ -25,25 +30,43 @@ def greet():
     name = 'Caitlin'
     return render_template( 'greet.html', 
     						title='Greetings',
-                            name=name
+                            name=name,
+                            health=c['health'],
+                            hunger=c['hunger'],
+                            stash=c['stash']
                           )
 
 @app.route('/intro')
 def intro():
+    c.update({'health': random.randint(-20, -1), 'hunger': random.randint(1,10)})
     return render_template('intro.html',
-                            title='Intro')
+                            title='Intro',
+                            health=c['health'],
+                            hunger=c['hunger'],
+                            stash=c['stash']
+                          )
 
 @app.route('/hatchet')
 def hatchet():
+    c.clear()
+    c.update({'health': (100 - random.randint(1,10)), 'hunger':random.randint(1,20), 'stash':'hatchet'})
     return render_template('hatchet.html',
-                            title='Hatchet')
+                            title='Hatchet',
+                            health=c['health'],
+                            hunger=c['hunger'],
+                            stash=c['stash']
+                          )
 
 @app.route('/apples')
 def apples():
     name = 'Caitlin'
+    c.update({'health': -100, 'hunger':0, 'stash':'Applesauce'})
     return render_template('apples.html',
                             title = 'Apples',
-                            name=name
+                            name=name,
+                            health=c['health'],
+                            hunger=c['hunger'],
+                            stash=c['stash']
                           )
 
 @app.route('/river')
